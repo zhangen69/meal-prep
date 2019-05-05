1b6a5bfee547e0aef168072b189c0c0527c3fbda
Jacob <zhangen69@gmail.com>
2019-05-06 00:13:28 +0800

    init

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDd3O0lNUNJm8BIQPV49P1Mi9cttPzvJbI",
+    authDomain: "https://meal-prep-fa4d7.firebaseapp.com/",
+    databaseURL: "",
+    projectId: "meal-prep-fa4d7",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+firebase.initializeApp(config);
